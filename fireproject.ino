309f6e7525130e636dea66ced1b9ad0928f60340
akshayasinganamala <akshayasinganamala@gmail.com>
2026-05-09 21:30:40 +0530

    Update fireproject.ino

--- fireproject.ino
+++ fireproject.ino
@@ -5,38 +5,42 @@
 #include <OneWire.h>
 #include <DallasTemperature.h>
 
-/************ WIFI ************/
+/**************** WIFI ****************/
 const char* ssid = "WIFI_NAME";
 const char* password = "WIFI_PASSWORD";
 
-/************ FLASK SERVER ************/
+/**************** FLASK SERVER ****************/
+// Replace with laptop IP
 const char* serverName =
 "http://192.168.1.5:5000";
 
-/************ DS18B20 ************/
+/**************** PIN CONNECTIONS ****************/
 #define ONE_WIRE_BUS 4
+#define GAS_PIN 34
+#define PIR_PIN 5
+#define BUZZER_PIN 18
 
+/**************** DS18B20 ****************/
 OneWire oneWire(ONE_WIRE_BUS);
 DallasTemperature sensors(&oneWire);
-
-/************ MQ SENSOR ************/
-#define MQ_PIN 34
-
-/************ PIR SENSOR ************/
-#define PIR_PIN 27
 
 void setup() {
 
   Serial.begin(115200);
 
+  /******** PIN MODES ********/
   pinMode(PIR_PIN, INPUT);
+  pinMode(BUZZER_PIN, OUTPUT);
 
+  digitalWrite(BUZZER_PIN, LOW);
+
+  /******** START TEMPERATURE SENSOR ********/
   sensors.begin();
 
   /******** WIFI CONNECT ********/
   WiFi.begin(ssid, password);
 
-  Serial.print("Connecting");
+  Serial.print("Connecting to WiFi");
 
   while (WiFi.status() != WL_CONNECTED) {
     delay(500);
@@ -44,37 +48,56 @@ void setup() {
   }
 
   Serial.println("\nWiFi Connected");
+  Serial.print("ESP32 IP: ");
+  Serial.println(WiFi.localIP());
 }
 
 void loop() {
 
-  /******** TEMPERATURE ********/
+  /**************** READ TEMPERATURE ****************/
   sensors.requestTemperatures();
 
   float temp =
     sensors.getTempCByIndex(0);
 
-  /******** GAS ********/
+  /**************** READ GAS SENSOR ****************/
   int gas =
-    analogRead(MQ_PIN);
+    analogRead(GAS_PIN);
 
-  /******** PERSON ********/
+  /**************** READ PIR SENSOR ****************/
   int person =
     digitalRead(PIR_PIN);
 
-  /******** SERIAL PRINT ********/
-  Serial.println("-----------");
+  /**************** PRINT VALUES ****************/
+  Serial.println("---------------");
 
-  Serial.print("Temp: ");
+  Serial.print("Temperature: ");
   Serial.println(temp);
 
-  Serial.print("Gas: ");
+  Serial.print("Gas Value: ");
   Serial.println(gas);
 
   Serial.print("Person: ");
   Serial.println(person);
 
-  /******** SEND TO FLASK ********/
+  /**************** BUZZER LOGIC ****************/
+  // Simple local alert
+
+  if (temp > 40 || gas > 1000) {
+
+    digitalWrite(BUZZER_PIN, HIGH);
+
+    Serial.println("BUZZER ON");
+  }
+
+  else {
+
+    digitalWrite(BUZZER_PIN, LOW);
+
+    Serial.println("BUZZER OFF");
+  }
+
+  /**************** SEND DATA TO FLASK ****************/
   if (WiFi.status() == WL_CONNECTED) {
 
     HTTPClient http;
@@ -86,7 +109,7 @@ void loop() {
       "application/json"
     );
 
-    /******** JSON ********/
+    /**************** JSON ****************/
     StaticJsonDocument<200> doc;
 
     doc["temp"] = temp;
@@ -97,18 +120,32 @@ void loop() {
 
     serializeJson(doc, jsonData);
 
-    /******** POST ********/
+    /**************** POST REQUEST ****************/
     int httpResponseCode =
       http.POST(jsonData);
 
-    Serial.print("HTTP Response: ");
-
+    Serial.print("HTTP Response Code: ");
     Serial.println(httpResponseCode);
+
+    if (httpResponseCode > 0) {
+
+      String response =
+        http.getString();
+
+      Serial.println("Server Response:");
+      Serial.println(response);
+    }
+
+    else {
+
+      Serial.println("Error Sending Data");
+    }
 
     http.end();
   }
 
   else {
+
     Serial.println("WiFi Disconnected");
   }
 
